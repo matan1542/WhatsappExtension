fd8be7caea35f103132a84abfadcdb7eed5d3178
Avior Kahalani <59415129+AviorKahalani@users.noreply.github.com>
2021-08-29 12:23:20 +0300

    Add event listener on button

--- popup.js
+++ popup.js
@@ -1,20 +1,9 @@
-// const phoneEl = document.querySelector('.phone-number');
-// const formEl = document.querySelector('whatsapp-form')
-// formEl.addEventListener('onsubmit', (ev) => {
-//     ev.preventDefault()
-//     console.log('Entering', ev)
-// })
+console.log('Start extension');
 
-document.addEventListener('DOMContentLoaded', function () {
-    const phoneEl = document.querySelector('.phone-number');
-    phoneEl.addEventListener('oninput', (ev) => {
-        console.log('Entering', ev)
-    })
-});
-function onInput({ target }) {
-    console.log('target.value',target.value)
+function send() {
+  let phoneNumber = document.querySelector('.phone-number').value;
+  let msg = document.querySelector('.msg').value;
+  alert(phoneNumber);
+  alert(msg);
 }
-function onSubmit(ev) {
-    ev.preventDefault()
-    console.log('event', ev)
-}
+document.getElementById('sbmt').addEventListener('click', send);
