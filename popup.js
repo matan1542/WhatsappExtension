94aef31d38a2db25f91d703935de83c002b61066
matan1542 <matan8380@gmail.com>
2021-08-29 13:02:06 +0300

    done

--- popup.js
+++ popup.js
@@ -1,9 +1,21 @@
-console.log('Start extension');
+document.getElementById('sbmt').addEventListener('click', send);
 
-function send() {
+async function send() {
   let phoneNumber = document.querySelector('.phone-number').value;
   let msg = document.querySelector('.msg').value;
-  alert(phoneNumber);
-  alert(msg);
+  const messageSend = await sendMessage(phoneNumber, msg)
+  alert(messageSend)
 }
-document.getElementById('sbmt').addEventListener('click', send);
+
+async function sendMessage(phoneNumber, msg) {
+  const apiKey = '5729ohyirgsv80r3'
+  try {
+      await axios.post(`https://api.chat-api.com/instance327514/sendMessage?token=${apiKey}`, {
+          "phone": phoneNumber,
+          "body": msg
+      })
+      return Promise.resolve('done')
+  } catch (err) {
+      return Promise.reject(err)
+  }
+}
